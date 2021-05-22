098188834f5e62c1face2c5c84b00e92b874f631
ryokoko <llzstella@yahoo.com>
2021-05-23 00:48:30 +1000

    Try using Flexbox to replace bootstrap

--- app/Page/HomePage/HomePage.js
+++ app/Page/HomePage/HomePage.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+/* import { Row, Col } from 'react-bootstrap'; */
 
 import styled from 'styled-components';
 import MainSection from '../components/MainSection';
 import Flex from '../../../Components/Flex';
 
 
-const HomeHeader = styled.div`
+const HomeHeader = styled(Flex)`
     background-image: url('./Images/title_bg.jpg');
     background-size: cover;
     background-repeat: no-repeat;
     border-radius: 10px 10px 0 0;
     padding: 30px;
+
+    @media (min-width: 768px) {
+        flex-direction: row;
+    }
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
+    
 `;
 
 const ProfilePhoto = styled.div`
     max-width: 200px;
     margin: 0 auto;
+    @media (min-width: 768px) {
+        flex: 40%;
+    }
+    @media (max-width: 768px) {
+        flex: 100%;
+    }
 `;
 
 const Avatar = styled.img`
@@ -27,6 +41,12 @@ const Avatar = styled.img`
 const TitleBlock = styled.div`
     text-align: center;
     padding: 2%;
+    @media (min-width: 768px) {
+        flex:60%;
+    }
+    @media (max-width: 768px) {
+        flex:100%;
+    }
 `;
 
 const SocialIcons = styled.div`
@@ -89,28 +109,20 @@ const HomePage = () => (
     <div>
         <MainSection>
                 <HomeHeader>                   
-                    <Row>
-                        <Col md={4}>
-                            <ProfilePhoto>
-                                <Avatar src="./Images/profile_img.jpg" alt="profile photo" />
-                            </ProfilePhoto>
-                        </Col>
-                        <Col md={8}>
-                            <TitleBlock>
-                                <h1>Rebecca Li</h1>
-                                <div class="title">Junior Radiographer</div>
-                            </TitleBlock>
-                            <SocialIcons>
-                                <a href="https://www.linkedin.com/in/liangzi-li-stella/" target="_blank" class="fa fa-linkedin" />
-                                <a href="https://github.com/ryokoko" target="_blank" class="fa fa-facebook" />
-                                <a href="https://www.instagram.com/ryoko.r_/" target="_blank" class="fa fa-instagram" />
-                            </SocialIcons>
-                        </Col>  
-                    </Row>
+                    <ProfilePhoto>
+                        <Avatar src="./Images/profile_img.jpg" alt="profile photo" />
+                    </ProfilePhoto>
+                    <TitleBlock>
+                        <h1>Rebecca Li</h1>
+                        <div class="title">Junior Radiographer</div>
+                    </TitleBlock>
+                    <SocialIcons>
+                        <a href="https://www.linkedin.com/in/liangzi-li-stella/" target="_blank" class="fa fa-linkedin" />
+                        <a href="https://github.com/ryokoko" target="_blank" class="fa fa-facebook" />
+                        <a href="https://www.instagram.com/ryoko.r_/" target="_blank" class="fa fa-instagram" />
+                    </SocialIcons>
                 </HomeHeader>
                 <HomeContent>
-                    <Row>
-                        <Col md={6}>
                             <div>
                                 <h3>About Me</h3>
                                 <div>Hi there! My name is Rebecca. I am a highly motivated 2020 radiography graduate from
@@ -118,8 +130,6 @@ const HomePage = () => (
                                 working in a team environment, whilst maintaining independence. I have worked in both 
                                 hospitals and fast-paced private practices.</div>
                             </div>                    
-                        </Col>
-                        <Col md={6}>
                             <PersonalDetails>
                                 <DetailItem>
                                     <DetailKey>Education</DetailKey><DetailValue>Associate of Applied Science: Diagnostic Radiography</DetailValue>
@@ -137,8 +147,6 @@ const HomePage = () => (
                                     <DetailKey>Address</DetailKey><DetailValue>123 Nonamelane, Box Hill VIC 3128</DetailValue>
                                 </DetailItem>
                             </PersonalDetails>
-                        </Col>
-                    </Row>
                 </HomeContent>
         </MainSection>     
     </div>
--- app/Page/Page.js
+++ app/Page/Page.js
@@ -10,15 +10,42 @@ const MainBody = styled.div`
     background-color: #e9e7e7;
 `;
 
-const Page = () => (
-    <div>
+
+const Page = () => {
+    const page ='HOME';
+
+    /* const pageMap = {
+        HOME: (<HomePage />),
+        RESUME: (<ResumePage />),
+        SERVICES: (<ServicesPage />),
+        CONTACT: (<ContactPage />),
+    }; 
+
+    const currentPage = pageMap[page]; */
+    /* <div>
         <MainBody>
             <HomePage />
             <ResumePage />
             <ServicesPage />
             <ContactPage />
         </MainBody> 
-    </div>
-)
+    </div> */
+    //return {currentPage}
+
+    //jsx中的boolean，null，undefined，[]值不会被render出来，而0可以
+    //{}不被接受
+    //因此下面的短路计算就是表达了falsy的页面是不会被渲染
+
+
+    return (
+        <div className="pages"> 
+            { page === 'HOME' && (<HomePage />) }
+            { page === 'RESUME' && (<ResumePage />) }
+            { page === 'SERVICES' && (<ServicesPage />) }
+            { page === 'CONTACT' && (<ContactPage />) }
+
+        </div>
+    )
+}
 
 export default Page;
